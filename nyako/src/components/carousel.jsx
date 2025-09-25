a35424c04fc2f8dbbee4d1c2f55802570931a78e
Wang <a4050010@gmail.com>
2025-09-25 20:47:38 +0800

    輪播圖大小縮放OK

--- nyako/src/components/carousel.jsx
+++ nyako/src/components/carousel.jsx
@@ -1,78 +1,37 @@
 "use client";
 import Image from "next/image";
 import { useState } from "react";
-// export default function Carousel() {
-//     let [slider, setSlider] = useState(0)
-//     return (
-//         <>
-//             <div className="relative overflow-hidden w-svw h-svh">
-//                 <div className="flex transition-transform duration-500"
-//                     style={{ transform: `translateX(-${slider * 100}svw)` }}>
-//                     <figure className="relative flex-none w-svw h-svh">
-//                         <Image src="/banner-01.png" fill alt="banner-01"  className="object-cover"/>
-//                     </figure>
-//                     <figure className="relative flex-none w-svw h-svh">
-//                         <Image src="/banner-02.jpg" fill alt="banner-02" className="object-cover" />
-//                     </figure>
-//                     <figure className="relative flex-none w-svw h-svh">
-//                         <Image src="/banner-03.jpg" fill alt="banner-03" className="object-cover" />
-//                     </figure>
-//                     <figure className="relative flex-none w-svw h-svh">
-//                         <Image src="/banner-04.jpg" fill alt="banner-04"  className="object-cover "/>
-//                     </figure>
-//                 </div>
-//                 <div className="absolute bottom-5 left-1/2 -translate-x-1/2 flex gap-2">
-//                     {[0, 1, 2, 3].map((i) => (
-//                         <button
-//                             key={i}
-//                             onClick={() => setSlider(i)}
-//                             className={`w-4 h-4 rounded-full ${slider === i ? "bg-white" : "bg-gray-400"
-//                                 }`}
-//                         ></button>
-//                     ))}
-//                 </div>
-//             </div>
-//         </>
-//     )
-// }
 export default function Carousel() {
-    const [slider, setSlider] = useState(0);
-    const banners = ["/banner-01.png", "/banner-02.jpg", "/banner-03.jpg", "/banner-04.jpg"];
-
+    let [slider, setSlider] = useState(0)
     return (
-
-
-        <div className="relative overflow-hidden w-full aspect-[9/16] md:aspect-[16/9] lg:h-dvh">
-            <div
-                className="flex h-full transition-transform duration-500 will-change-transform"
-                style={{ transform: `translate3d(-${slider * 100}%, 0, 0)` }}
-            >
-                {banners.map((src, i) => (
-                    <figure key={i} className="relative w-full h-full shrink-0">
-                        <Image
-                            src={src}
-                            alt={`banner-${i + 1}`}
-                            fill
-                            className="object-contain object-top md:object-cover"
-                            sizes="(min-width: 768px) 100vw, 100vw"
-                            quality={85}
-                            priority={i === 0}
-                        />
+        <>
+            <div className="relative overflow-hidden w-full aspect-[2/1] md:aspect-[2/1]">
+                <div className="flex w-full h-full transition-transform duration-500"
+                    style={{ transform: `translateX(-${slider * 100}svw)` }}>
+                    <figure className="relative flex-none w-svw h-auto">
+                        <Image src="/banner-01.png" width={960} height={480} alt="banner-01" className="md:w-full h-full  object-cover" />
                     </figure>
-                ))}
+                    <figure className="relative flex-none w-svw h-auto">
+                        <Image src="/banner-02.jpg" width={960} height={480} alt="banner-02" className="md:w-full h-full  object-cover" />
+                    </figure>
+                    <figure className="relative flex-none w-svw h-auto">
+                        <Image src="/banner-03.jpg" width={960} height={480} alt="banner-03" className="md:w-full h-full  object-cover" />
+                    </figure>
+                    <figure className="relative flex-none w-svw h-auto">
+                        <Image src="/banner-04.jpg" width={960} height={480} alt="banner-04" className="md:w-full h-full  object-cover " />
+                    </figure>
+                </div>
+                <div className="absolute bottom-5 left-1/2 -translate-x-1/2 flex gap-2">
+                    {[0, 1, 2, 3].map((i) => (
+                        <button
+                            key={i}
+                            onClick={() => setSlider(i)}
+                            className={`w-4 h-4 rounded-full ${slider === i ? "bg-white" : "bg-gray-400"
+                                }`}
+                        ></button>
+                    ))}
+                </div>
             </div>
-            <div className="absolute bottom-5 left-1/2 -translate-x-1/2 flex gap-2">
-                {[0, 1, 2, 3].map((i) => (
-                    <button
-                        key={i}
-                        onClick={() => setSlider(i)}
-                        className={`w-4 h-4 rounded-full ${slider === i ? "bg-white" : "bg-gray-400/70"}`}
-                        aria-label={`Go to slide ${i + 1}`}
-                    />
-                ))}
-            </div>
-        </div>
-
-    );
+        </>
+    )
 }
-
--- nyako/src/components/event.jsx
+++ nyako/src/components/event.jsx
@@ -2,29 +2,29 @@ import { Span } from "next/dist/trace";
 import Image from "next/image";
 import photo from "../../public/event/island.jpg"
 
-export default function Event(){
-    
-    return(
+export default function Event() {
+
+    return (
         <>
-        <main className="bg-white w-1/2">
-            <span className="text-3xl">活動關卡</span>
-            <div className="grid grid-rows-3">
-                <div className="flex flex-1">
-                    <Image src="/event/island.jpg" width={300} height={150}/>
-                    <div>
-                        <span>可獲取角色</span>
-                        <div className="flex">
-                            <Image src="/cat/EX/summer-01.png" width={104} height={79}/>
-                            <Image src="/cat/EX/summer-02.png" width={104} height={79}/>
-                            <Image src="/cat/EX/summer-03.png" width={104} height={79}/>
-                            <Image src="/cat/EX/summer-04.png" width={104} height={79}/>
+            <main className="bg-white w-1/2">
+                <span className="text-3xl">活動關卡</span>
+                <div className="grid grid-rows-3">
+                    <div className="flex flex-1">
+                        <Image src="/event/island.jpg" width={300} height={150} alt="貓咪的暑假" />
+                        <div>
+                            <span>可獲取角色</span>
+                            <div className="flex">
+                                <Image src="/cat/EX/summer-01.png" width={104} height={79} alt="扇貝貓" />
+                                <Image src="/cat/EX/summer-02.png" width={104} height={79} alt="沙丁魚" />
+                                <Image src="/cat/EX/summer-03.png" width={104} height={79} alt="烏賊" />
+                                <Image src="/cat/EX/summer-04.png" width={104} height={79} alt=" 巨大擬濱蟹" />
+                            </div>
                         </div>
                     </div>
+                    <div className="flex flex-1"></div>
+                    <div className="flex flex-1"></div>
                 </div>
-                <div className="flex flex-1"></div>
-                <div className="flex flex-1"></div>
-            </div>
-        </main>
+            </main>
         </>
     )
 }
